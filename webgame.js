b2591828a3ee0c86282bc160c26a329acd14753c
ang3line <ang3line.sia@gmail.com>
2023-06-29 09:02:31 +1200

    hh

--- webgame.js
+++ webgame.js
@@ -1,2 +1,23 @@
 const canvas = document.getElementById('game');
-const ctx = 
+const ctx = canvas.getContext("2d")
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 200;
+
+let scaleRatio = null ; 
+
+function setScreen(){ 
+    scaleRatio = getScaleRatio();
+}
+
+function getScaleRatio(){ 
+    const screenHeight = Math.min( 
+    window.innerHeight, 
+    document.documentElement.clientHeight
+    );
+
+    const screenWidth = Math.min(
+    window.innerWidth, 
+    document.documentElement.clientWidth
+    );
+}
